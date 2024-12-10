76f58a581846b6faa7a84e866ef02d6f7b38e954
Dong-Han <donghk77@naver.com>
2024-12-10 19:18:42 +0900

    feat : add literatureCreateView

--- v1/posts/views/literature.py
+++ v1/posts/views/literature.py
@@ -4,53 +4,64 @@ from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 
-from ..models.literature import Post
-from ..serializers.literature import PostSerializer, PostCreateSerializer, PostUpdateSerializer
+from ..models.literature import Literature
+from ..serializers.literature import LiteratureSerializer, LiteratureCreateSerializer, LiteratureUpdateSerializer
 
 
-class PostView(APIView):
+class LiteratureView(APIView):
     
     @staticmethod
     def get(request):
         '''
         all posts's list
         '''
-        posts = Post.objects.all()
-        return Response(PostSerializer(posts, many=True).data)
+        literatures = Literature.objects.all()
+        return Response(LiteratureSerializer(literatures, many=True).data)
     
     @staticmethod
     def post(request):
         '''
         create
         '''
-        serializer = PostCreateSerializer(data=request.data, context={'request': request})
+        serializer = LiteratureCreateSerializer(data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
-            return Response(PostSerializer(serializer.instance).data, status=status.HTTP_201_CREATED)
+            return Response(LiteratureSerializer(serializer.instance).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class PostDetailView(APIView):
+class LiteratureDetailView(APIView):
     
     @staticmethod
     def get(request, post_id):
         '''
         view individual post
         '''
-        post = get_object_or_404(Post, pk=post_id)
-        return Response(PostSerializer(post).data)
+        literature = get_object_or_404(Literature, pk=post_id)
+        return Response(LiteratureSerializer(literature).data)
     
     @staticmethod
     def put(request, post_id):
         '''
         update post
         '''
-        post = get_object_or_404(Post, pk=post_id)
+        post = get_object_or_404(Literature, pk=post_id)
     
     @staticmethod
     def delete(request, post_id):
-        post = get_object_or_404(Post, pk=post_id)
+        post = get_object_or_404(Literature, pk=post_id)
         if post.user != request.user:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         post.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class LiteratureCreateView(APIView):
+
+    def post(self, request):
+        """
+        Create a new Literature post.
+        """
+        serializer = LiteratureCreateSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(LiteratureSerializer(serializer.instance).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
