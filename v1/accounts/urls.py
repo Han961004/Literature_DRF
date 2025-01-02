2726eb905e7808446f262020f4d83b34cf9d2a7e
Dong-Han <donghk77@naver.com>
2025-01-02 22:37:19 +0900

    feat : TEST

--- v1/accounts/urls.py
+++ v1/accounts/urls.py
@@ -7,5 +7,5 @@ from .views.user import *
 urlpatterns = [
     path('v1/accounts/create/', UserView.as_view()),
     path('v1/accounts/login/', LoginView.as_view()),
-    path('v1/accounts/logout111/', LogoutView.as_view()),
+    path('v1/accounts/logout1111/', LogoutView.as_view()),
 ]
